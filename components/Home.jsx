a910d3901b632ec536acea816c9ce4d877a78196
Mercedes <mercedeschoy@gmail.com>
2021-07-26 16:46:35 +0100

    Add email and password input for login

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { Input, Button } from "react-native-elements";
-import { Alert } from "react-native";
+import { useState } from "react";
 
-const Home = () => {
+const Home = ({ setUser }) => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handlePassword = (e) => {
+    const newLogin = { ...login };
+    newLogin.password = e;
+    setLogin(newLogin);
+  };
+
+  const handleEmail = (e) => {
+    const newLogin = { ...login };
+    newLogin.email = e;
+    setLogin(newLogin);
+  };
+
+  console.log(login);
+
   return (
     <>
       <Input
         placeholder="Enter your email address"
         leftIcon={{ type: "font-awesome", name: "envelope" }}
+        onChangeText={(e) => handleEmail(e)}
+        value={login.email}
       />
 
       <Input
         placeholder="Password"
         secureTextEntry={true}
         leftIcon={{ type: "font-awesome", name: "key" }}
+        onChangeText={(e) => handlePassword(e)}
+        value={login.password}
       />
       <Button title="Login" backgroundColor="red"></Button>
     </>
