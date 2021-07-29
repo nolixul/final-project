34ce7064085a28397ec3d05b7b4d42e905c3e5e6
nolixul <nadia.kaye@gmail.com>
2021-07-29 16:40:05 +0100

    Add Organisation sign up form and Volunteer sign up form, add non-working toggle to signup component

---
+++ src/components/signup_components/OrganisationSUForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View } from "react-native";
+import { Input } from "react-native-elements";
+
+const OrganisationSUForm = () => {
+	return (
+		<View>
+			<Input
+				placeholder='Organisation Name'
+				leftIcon={{ type: "font-awesome", name: "user" }}
+				/* onChangeText={(e) => handleEmail(e)} */
+				/* value={login.email} */
+			/>
+
+			<Input
+				placeholder='Enter your email address'
+				leftIcon={{ type: "font-awesome", name: "envelope" }}
+				/*  onChangeText={(e) => handleEmail(e)} */
+				/* value={login.email} */
+			/>
+
+			<Input
+				placeholder='Password'
+				secureTextEntry={true}
+				leftIcon={{ type: "font-awesome", name: "key" }}
+				/* onChangeText={(e) => handlePassword(e)} */
+				/* value={login.password} */
+			/>
+		</View>
+	);
+};
+
+export default OrganisationSUForm;
