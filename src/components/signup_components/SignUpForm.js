c951c84ec17d37161fd8b2a587399842f6764b1a
iqpalm <iqpalm@hotmail.com>
2021-07-29 11:58:08 +0100

    add layout and view tag to SignUpForm

--- src/components/signup_components/SignUpForm.js
+++ src/components/signup_components/SignUpForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Input, Button, CheckBox } from "react-native-elements";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 // Volunteer sign up form page
 
@@ -12,78 +12,80 @@ const SignUpForm = () => {
 
 	return (
 		<>
-			{/* CHOOSE AVATAR */}
+			<View style={{ flex: 1, justifyContent: "center" }}>
+				{/* CHOOSE AVATAR */}
 
-			<Input
-				placeholder="First Name"
-				leftIcon={{ type: "font-awesome", name: "user" }}
-				/* onChangeText={(e) => handleEmail(e)} */
-				/* value={login.email} */
-			/>
-			<Input
-				placeholder="Surname"
-				leftIcon={{ type: "font-awesome", name: "envelope" }}
-				/*  onChangeText={(e) => handleEmail(e)} */
-				/* value={login.email} */
-			/>
+				<Input
+					placeholder="First Name"
+					leftIcon={{ type: "font-awesome", name: "user" }}
+					/* onChangeText={(e) => handleEmail(e)} */
+					/* value={login.email} */
+				/>
+				<Input
+					placeholder="Surname"
+					leftIcon={{ type: "font-awesome", name: "envelope" }}
+					/*  onChangeText={(e) => handleEmail(e)} */
+					/* value={login.email} */
+				/>
 
-			<Input
-				placeholder="Enter your email address"
-				leftIcon={{ type: "font-awesome", name: "envelope" }}
-				/*  onChangeText={(e) => handleEmail(e)} */
-				/* value={login.email} */
-			/>
+				<Input
+					placeholder="Enter your email address"
+					leftIcon={{ type: "font-awesome", name: "envelope" }}
+					/*  onChangeText={(e) => handleEmail(e)} */
+					/* value={login.email} */
+				/>
 
-			<Input
-				placeholder="Password"
-				secureTextEntry={true}
-				leftIcon={{ type: "font-awesome", name: "key" }}
-				/* onChangeText={(e) => handlePassword(e)} */
-				/* value={login.password} */
-			/>
+				<Input
+					placeholder="Password"
+					secureTextEntry={true}
+					leftIcon={{ type: "font-awesome", name: "key" }}
+					/* onChangeText={(e) => handlePassword(e)} */
+					/* value={login.password} */
+				/>
 
-			<Text>Interests</Text>
-			<CheckBox
-				title="Community support"
-				checkedIcon="dot-circle-o"
-				uncheckedIcon="circle-o"
-				onPress={() => {
-					setCheckedCS(!checkedCS);
-				}}
-				checked={checkedCS}
-			/>
-			<CheckBox
-				title="Mental Health"
-				checkedIcon="dot-circle-o"
-				uncheckedIcon="circle-o"
-				onPress={() => {
-					setCheckedMH(!checkedMH);
-				}}
-				checked={checkedMH}
-			/>
-			<CheckBox
-				title="Environment"
-				checkedIcon="dot-circle-o"
-				uncheckedIcon="circle-o"
-				onPress={() => {
-					setCheckedEN(!checkedEN);
-				}}
-				checked={checkedEN}
-			/>
-			<CheckBox
-				title="Animals"
-				checkedIcon="dot-circle-o"
-				uncheckedIcon="circle-o"
-				onPress={() => {
-					setCheckedAN(!checkedAN);
-				}}
-				checked={checkedAN}
-			/>
-			<Button
-				title="Sign Up"
-				backgroundColor="red"
-				/*   onPress={handleSubmit} */
-			></Button>
+				<Text>Interests</Text>
+				<CheckBox
+					title="Community support"
+					checkedIcon="dot-circle-o"
+					uncheckedIcon="circle-o"
+					onPress={() => {
+						setCheckedCS(!checkedCS);
+					}}
+					checked={checkedCS}
+				/>
+				<CheckBox
+					title="Mental Health"
+					checkedIcon="dot-circle-o"
+					uncheckedIcon="circle-o"
+					onPress={() => {
+						setCheckedMH(!checkedMH);
+					}}
+					checked={checkedMH}
+				/>
+				<CheckBox
+					title="Environment"
+					checkedIcon="dot-circle-o"
+					uncheckedIcon="circle-o"
+					onPress={() => {
+						setCheckedEN(!checkedEN);
+					}}
+					checked={checkedEN}
+				/>
+				<CheckBox
+					title="Animals"
+					checkedIcon="dot-circle-o"
+					uncheckedIcon="circle-o"
+					onPress={() => {
+						setCheckedAN(!checkedAN);
+					}}
+					checked={checkedAN}
+				/>
+				<Button
+					title="Sign Up"
+					backgroundColor="red"
+					/*   onPress={handleSubmit} */
+				></Button>
+			</View>
 		</>
 	);
 };
