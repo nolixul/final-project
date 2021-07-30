2485ba95190b0c2a9309574bb3eda7339a8ec0ab
nolixul <nadia.kaye@gmail.com>
2021-07-30 18:07:00 +0100

    Add Profile component simple layout, add avatar to vol profile

---
+++ src/components/profile_components/VolProfile.js
@@ -0,0 +1,13 @@
+import { Avatar, Button } from "@ui-kitten/components";
+import React from "react";
+
+const VolProfile = () => {
+	return (
+		<>
+			<Avatar source={require("../../images/")} />
+			<Button>NAME PLACEHOLDER</Button>
+		</>
+	);
+};
+
+export default VolProfile;
