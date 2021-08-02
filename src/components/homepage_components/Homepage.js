7dedf4026649ff86db40a2fe67cb1bc0a6f89a76
iqpalm <iqpalm@hotmail.com>
2021-08-02 14:22:43 +0100

    add meaningful names to application files

--- src/components/homepage_components/Homepage.js
+++ src/components/homepage_components/Homepage.js
@@ -1,34 +1,15 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-	Divider,
-	Icon,
-	Layout,
-	TopNavigation,
-	TopNavigationAction
-} from "@ui-kitten/components";
+import { Divider, Layout } from "@ui-kitten/components";
 import { DrawerNavigation } from "../../navigation/drawernavigation";
-
-const BackIcon = (props) => <Icon {...props} name='arrow-back' />;
+import CustomHeader from "../CustomHeader";
 
 // Homepage
-const Homepage = ({ navigation }) => {
-	const navigateBack = () => {
-		navigation.goBack();
-	};
-
-	const BackAction = () => (
-		<TopNavigationAction icon={BackIcon} onPress={navigateBack} />
-	);
-
+const Homepage = () => {
 	return (
 		<>
 			<SafeAreaView style={{ flex: 1 }}>
-				<TopNavigation
-					title='MyApp'
-					alignment='center'
-					accessoryLeft={BackAction}
-				/>
+				<CustomHeader />
 				<Divider />
 				<Layout style={{ flex: 1 }}>
 					<DrawerNavigation />
--- src/navigation/stackNavRoutes.js
+++ src/navigation/stackNavRoutes.js
@@ -2,12 +2,10 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../components/Login";
 import Homepage from "../components/homepage_components/Homepage";
-import VolHomepage from "../components/homepage_components/VolHomepage";
-import OrgHomepage from "../components/homepage_components/OrgHomepage";
-import VolApplication from "../components/applications_components/VolApplication";
-import OrgApplication from "../components/applications_components/OrgApplication";
+import VolApplicationOverview from "../components/applications_components/VolApplicationsOverview";
+import OrgOpportunitiesOverview from "../components/applications_components/OrgOpportunitiesOverview";
 import VolMakeApplication from "../components/make_applications_components/VolMakeApplication";
-import OrgMakeApplication from "../components/make_applications_components/OrgMakeApplication";
+import OrgPostOpportunity from "../components/make_applications_components/OrgPostOpportunity";
 import SignUpForm from "../components/signup_components/SignUpForm";
 import OrganisationSUForm from "../components/signup_components/OrganisationSUForm";
 import VolunteerSUForm from "../components/signup_components/VolunteerSUForm";
@@ -20,12 +18,13 @@ export const LoginNavigator = () => (
 	<Navigator headerMode="none">
 		<Screen name="Login" component={Login} />
 		<Screen name="Homepage" component={Homepage} />
-		<Screen name="VolHomepage" component={VolHomepage} />
-		<Screen name="OrgHomepage" component={OrgHomepage} />
-		<Screen name="VolApplication" component={VolApplication} />
-		<Screen name="OrgApplication" component={OrgApplication} />
+		<Screen name="VolApplicationOverview" component={VolApplicationOverview} />
+		<Screen
+			name="OrgOpportunitiesOverview"
+			component={OrgOpportunitiesOverview}
+		/>
 		<Screen name="VolMakeApplication" component={VolMakeApplication} />
-		<Screen name="OrgMakeApplication" component={OrgMakeApplication} />
+		<Screen name="OrgPostOpportunity" component={OrgPostOpportunity} />
 		<Screen name="SignUpForm" component={SignUpForm} />
 		<Screen name="VolunteerSUForm" component={VolunteerSUForm} />
 		<Screen name="OrganisationSUForm" component={OrganisationSUForm} />
